63caa0977f3f05b2b6ac44c804f9cb9f6ce21064
Ryan Sheridan <0xryann@protonmail.com>
2025-03-26 20:23:18 +0000

    register validation

--- public/js/login.js
+++ public/js/login.js
@@ -49,20 +49,84 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/;
+    const usernameRegex = /^[a-z0-9_-]{4,}$/;
+    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+
+    const registerUsernameInput = document.querySelector("#registerUsername");
+    const registerEmailInput = document.querySelector("#registerEmail");
+    const registerPasswordInput = document.querySelector("#registerPassword");
+
+    // validate input real time, also convert to lowercase
+    registerUsernameInput.addEventListener('input', function () {
+        this.value = this.value.toLowerCase();
+
+        if (usernameRegex.test(this.value)) {
+            this.style.border = "";
+        } else {
+            this.style.border = "1px solid red";
+        }
+    });
+
+    // validate input real time
+    registerEmailInput.addEventListener('input', function () {
+        if (emailRegex.test(this.value.trim())) {
+            this.style.border = "";
+        } else {
+            this.style.border = "1px solid red";
+        }
+    });
+
+    // validate input real time
+    registerPasswordInput.addEventListener('input', function () {
+        if (passwordRegex.test(this.value)) {
+            this.style.border = "";
+        } else {
+            this.style.border = "1px solid red";
+        }
+    });
+
+    // handle register submission
     registerSubmit?.addEventListener("click", async function (e) {
         e.preventDefault();
-        const username = document.querySelector("#registerUsername").value.trim();
-        const email = document.querySelector("#registerEmail").value.trim();
-        const password = document.querySelector("#registerPassword").value;
+
+        const username = registerUsernameInput.value.trim();
+        const email = registerEmailInput.value.trim();
+        const password = registerPasswordInput.value;
+
+        // ensure all fields are filled
         if (!username || !email || !password) {
             alert('Please fill in all registration fields');
             return;
         }
-        const message = await registerUser(username, email, password);
-        if (message) {
-            alert(message);
+
+        // validate each field before submitting
+        if (!usernameRegex.test(username)) {
+            alert('invalid username');
+            registerUsernameInput.style.border = "1px solid red";
+            return;
+        }
+        if (!emailRegex.test(email)) {
+            alert('invalid email');
+            registerEmailInput.style.border = "1px solid red";
+            return;
+        }
+        if (!passwordRegex.test(password)) {
+            alert('invalid password');
+            registerPasswordInput.style.border = "1px solid red";
+            return;
+        }
+
+        try {
+            const { message } = await registerUser(username, email, password);
+            console.log(message);
+            window.location = '/login';
+        } catch (error) {
+            console.error(error);
+            alert('register failed ' + (error.message || error));
         }
     });
+
 
     resetButton?.addEventListener("click", async function (e) {
         e.preventDefault();
