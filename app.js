02eeef159e0d324ddaa5687974a88e97a74ab66a
Iga Walczynska <x23400226@student.ncirl.ie>
2025-04-06 19:09:05 +0100

    added change user name option (not fully functional yet)and cleaned up the ui of spotify success/failure pages

--- app.js
+++ app.js
@@ -170,3 +170,19 @@ app.get('/reauth', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+app.post('/profile/update-username', async (req, res) => {
+  const { username } = req.body;
+
+  if (!username) {
+    return res.status(400).send('Username is required');
+  }
+
+  try {
+    await UserModel.updateUsername(req.user.id, username);
+    res.status(200).send('Username updated successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error updating username');
+  }
+});
